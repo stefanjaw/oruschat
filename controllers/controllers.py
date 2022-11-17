12b9c3b58b6f1e84c2d46ea573303c4a76c7b391
Stefan J <stefanj@avalantec.com>
2022-11-17 22:18:41 +0000

    Updated

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -10,8 +10,6 @@ class Oruschat(http.Controller):
 
     @http.route('/oruschat/lead', auth='public', csrf=False, methods=['POST'], type='json')
     def oruschat_post(self, **kw):
-        
-        #Falta la validacion dek key
 
         
         # data = (json.loads((http.request.httprequest.data).decode('utf-8'))).get('data')
@@ -20,7 +18,6 @@ class Oruschat(http.Controller):
         
         args = http.request.httprequest.args
         
-        _logging.info(f"DEF15 data: {data}\n")
 
         status_response = True
         
@@ -37,18 +34,8 @@ class Oruschat(http.Controller):
         source = data.get('source')
         active = data.get('active')
         
-        #Se utiliza para realizar las validaciones y que el codigo este mas limpio
         aux_null = [False, None, ""]
-        
-        
-        if  category_name in aux_null:
-            category_name_id = ""
-        else:
-            category_name_id = (http.request.env['crm.tag'].sudo().search([
-            ('name', 'ilike', category_name )
-            ])).id
-        
-        _logging.info(f"DEF49 ========")
+        lead_data = {}
         
         if oruschat_id in aux_null:
             data = {
@@ -65,71 +52,72 @@ class Oruschat(http.Controller):
             ('oruschat_id', '=', oruschat_id)
             ]))
         
-        _logging.info(f"DEF67=======")
-        if product_name in aux_null:
-            product_id = ""
-        else:
-            product_id = (http.request.env['product.product'].sudo().search([
-            ('name', 'ilike', product_name)
-            ]))
         
         
-        if agent_email in aux_null:
-            user_id = ""
-        else:
-            user_id = (http.request.env['res.users'].sudo().search([
-            ('partner_id.email', '=', agent_email)
+        if  category_name not in aux_null:
+            category_name_id = (http.request.env['crm.tag'].sudo().search([
+            ('name', 'ilike', category_name )
             ]))
-            
-        _logging.info(f"DEF83======")
-        if source in aux_null:
-            source_id = ""
-        else:
-            source_id = (http.request.env['utm.source'].sudo().search([
-            ('name', 'ilike', source)
-            ]))
-        
-        _logging.info(f"DEF91======")
-        if medium in aux_null:
-            medium_id = ""
-        else:
-            medium_id = (http.request.env['utm.medium'].sudo().search([
-            ('name', 'ilike', medium)
-            ]))
-            
-        
-        _logging.info(f"DEF100====== partner_ids: {partner_ids}")
-        #for partner_id in partner_ids:
-        #    _logging.info(f"DEF102======")
-        lead_id = http.request.env['crm.lead'].sudo().create({
-                'name' : lead_name,
-                #'tag_ids' : category_name_id.id,
-                'product_id' : product_id.id,
-                'contact_name' : contact_name,
-                #'partner_id' : partner_id.id,
-                'user_id' : user_id.id,
-                'email_from' : email,
-                'phone' : phone,
+            if category_name_id not in aux_null:
+                lead_data['tag_ids'] = category_name_id.id 
 
-                'medium_id' : medium_id.id,
-                'source_id' : source_id.id,
-                'active' : active,
-        })
-        _logging.info(f"DEF117====== lead_id: {lead_id}")
-        _logging.info(f"DEF118====== ")
+        if  product_name not in aux_null:
+            product_id = (http.request.env['product.product'].sudo().search([
+            ('name', 'ilike', product_name )
+            ]))
+            if product_id not in aux_null:
+                lead_data['product_id'] = product_id.id 
+        
+        if  agent_email not in aux_null:
+            user_id = (http.request.env['res.users'].sudo().search([
+            ('name', '=', agent_email )
+            ]))
+            if user_id not in aux_null:
+                lead_data['user_id'] = user_id.id 
+        
+        if  source not in aux_null:
+            source_id = (http.request.env['utm.source'].sudo().search([
+            ('name', 'ilike', source )
+            ]))
+            if user_id not in aux_null:
+                lead_data['source_id'] = source_id.id 
+        
+        if  medium not in aux_null:
+            medium_id = (http.request.env['utm.medium'].sudo().search([
+            ('name', 'ilike', medium )
+            ]))
+            if user_id not in aux_null:
+                lead_data['medium_id'] = medium_id.id
+        
+        
+        
+        if  lead_name not in aux_null:
+            lead_data['lead_name'] = lead_name
+        
+        if  contact_name not in aux_null:
+            lead_data['contact_name'] = contact_name
+            
+        if  email not in aux_null:
+            lead_data['email_from'] = email
+        
+        if  phone not in aux_null:
+            lead_data['phone'] = phone
+            
+        if  active not in aux_null:
+            lead_data['active'] = active
+        
+        
+        for partner_id in partner_ids:
+            lead_id = http.request.env['crm.lead'].sudo().create(lead_data)
         
         data = {
             'status' : True
         }
         
-        headers = {
-            'content-type' : 'application/json'
-        }
-        
-        status=200
-        response = "ALGO"  # json.dumps(data)
-        
-        return http.Response( response,status,headers, )
+        http.Response.status = "400 algo"
+        return json.dumps(data) #http.Response( response,status,headers, )
+
+
         
         
         
